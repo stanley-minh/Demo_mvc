61e08a4b6306da9e6f0f2d410f2e81bafa7bc53f
stanley-minh <staley.bullet.pro@gmail.com>
2026-08-20 14:13:35 +0200

    Correction Exercice05 : controllerUser en POO

--- controller/controllerUsers.php
+++ controller/controllerUsers.php
@@ -1,16 +1,75 @@
 <?php
 //CONTROLLER
+class ControllerUser{
+    //ATTRIBUTS
+    private ModelUser $modelUser;
+    private ?ViewUser $viewUser;
+    private ?string $titre;
 
-function displayUsers(){
-    //Creation d'un objet ModelUser
-    $modelUser = new ModelUser(connect());
+    //CONSTRUCTEUR
+    public function __construct(ModelUser $model){
+        $this->modelUser = $model;
+    }
 
-    //Appel du model pour récupération des données
-    $data = $modelUser->findAll();
+    //GETTER ET SETTER
+    public function setTitre($newTitre):self{
+        $this->titre = newTitre;
+        return $this;
+    }
+    public function getTitre(){
+        return $this->titre;
+    }
+    /**
+     * Get the value of modelUser
+     *
+     * @return ModelUser
+     */
+    public function getModelUser(): ModelUser {
+        return $this->modelUser;
+    }
 
-    //Appel de la view pour effectuer l'affichage
-    $title = "Mes Utilisateurs";
-    include('./view/viewHeader.php');
-    include('./view/viewUser.php');
-    include('./view/viewFooter.php');
+    /**
+     * Set the value of modelUser
+     *
+     * @param ModelUser $modelUser
+     *
+     * @return self
+     */
+    public function setModelUser(ModelUser $modelUser): self {
+        $this->modelUser = $modelUser;
+        return $this;
+    }
+
+    /**
+     * Get the value of viewUser
+     *
+     * @return ?ViewUser
+     */
+    public function getViewUser(): ?ViewUser {
+        return $this->viewUser;
+    }
+
+    /**
+     * Set the value of viewUser
+     *
+     * @param ?ViewUser $viewUser
+     *
+     * @return self
+     */
+    public function setViewUser(?ViewUser $viewUser): self {
+        $this->viewUser = $viewUser;
+        return $this;
+    }
+
+    //METHODS
+    public function render(){
+        //Appel du model pour récupération des données
+        $data = $this->modelUser->findAll();
+
+        //Appel de la view pour effectuer l'affichage
+        $title = "Mes Utilisateurs";
+        include('./view/viewHeader.php');
+        include('./view/viewUser.php');
+        include('./view/viewFooter.php');
+    }
 }
