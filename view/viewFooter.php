ae753a27586a90efb9f724496a160793ab649931
stanley-minh <staley.bullet.pro@gmail.com>
2026-08-24 16:43:05 +0200

    Demo Bufferisation avec ob_start et ob_get_clean : fichier viewFooter.php

--- view/viewFooter.php
+++ view/viewFooter.php
@@ -1,10 +1,27 @@
 <?php
 class ViewFooter{
+    //ATTRIBUT
+    private ?string $buffer;
+
     //METHODS
+    //methode de mise en mémoire tampon
+    public function launchBuffer():self{
+        ob_start(); //Mise en mémoire tampon (buffer)
+?>
+            <footer>
+               <p> <?php echo "Salut le Monde !" ?> </p>
+            </footer>
+        </body>
+        </html>
+<?php 
+        $this->buffer = ob_get_clean(); //récupération le contenu du buffer et j'efface le buffer
+
+        return $this;
+    }
+
+    //methode d'affiche du contenu HTML
     public function display():void{
-        echo "<footer></footer>
-            </body>
-            </html>";
+        echo $this->buffer; //affichage du contenu en mémoire tampon
     }
 }
 
