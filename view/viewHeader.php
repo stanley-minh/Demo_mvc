1a2f77dff90340c318ed6e9fce5e94ab2285b5aa
stanley-minh <staley.bullet.pro@gmail.com>
2026-08-24 11:14:45 +0200

    Correction Exercice 6 Part 2 : ViewHeader en POO

--- view/viewHeader.php
+++ view/viewHeader.php
@@ -1,25 +1,33 @@
 <?php
-function nav(){
-    echo`<nav>
-            <a href="/MVC/">Utilisateurs</a>
-            <a href="/MVC/articles">Articles</a>
-        </nav>`;
-}
+//Class ViewHeader
+class ViewHeader{
+    //ATTRIBUTS
+    private ?string $title;
+
+    //CONSTRUCTOR
+    public function __construct(?string $title = "Mon Super Site"){
+        $this->title = $title;
+    }
+
+    //GETTER ET SETTER
+
+    //METHOD
+    public function display(){
+        echo '<!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>'.$this->title.'</title>
+            </head>
+            <body>
+                <header>
+                    <nav>
+                        <a href='.$_ENV['utilisateurs'].'>Utilisateurs</a>
+                        <a href='.$_ENV['articles'].'>Articles</a>
+                    </nav>
+                </header>';
+                }
+            }
 
 ?>
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo $title ?></title>
-</head>
-<body>
-    <header>
-        <nav>
-            <a href=<?php echo $_ENV['utilisateurs'] ?>>Utilisateurs</a>
-            <a href=<?php echo $_ENV['articles'] ?>>Articles</a>
-        </nav>
-    </header>
