62c0db09bca0cbc08d41923c1f63d08f5683242b
stanley-minh <staley.bullet.pro@gmail.com>
2026-08-25 11:12:25 +0200

    Update avant migration

--- utils/utils.php
+++ utils/utils.php
@@ -1,6 +1,6 @@
 <?php
 function connect(){
-    return new PDO('mysql:host=127.0.0.1:3306;dbname=mvc','root','root',[
+    return new PDO('mysql:host=127.0.0.1:3306;dbname=mvc','root','',[
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
     ]);
 }
--- view/viewHeader.php
+++ view/viewHeader.php
@@ -1,37 +1,42 @@
 <?php
-//Class ViewHeader
-class ViewHeader{
-    //ATTRIBUTS
-    private ?string $title;
-    private ?string $linkScript;
+class ViewHeader
+{
+    private ?string $buffer = null;
 
-    //CONSTRUCTOR
-    public function __construct(?string $title = "Mon Super Site", ?string $linkScript = ''){
-        $this->title = $title;
-        $this->linkScript = $linkScript;
+    public function __construct(
+        private ?string $title = "Mon Super Site",
+        private ?string $linkScript = ''
+    ) {}
+
+    public function launchBuffer(): self
+    {
+        ob_start();
+        ?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= htmlspecialchars($this->title ?? '') ?></title>
+    <link rel="stylesheet" href="./public/src/css/style.css">
+    <?php if (!empty($this->linkScript)): ?>
+    <script src="<?= htmlspecialchars($this->linkScript) ?>" defer></script>
+    <?php endif; ?>
+</head>
+<body>
+    <header>
+        <nav>
+            <a href="<?= htmlspecialchars($_ENV['utilisateurs'] ?? '#') ?>">Utilisateurs</a>
+            <a href="<?= htmlspecialchars($_ENV['articles'] ?? '#') ?>">Articles</a>
+        </nav>
+    </header>
+        <?php
+        $this->buffer = ob_get_clean();
+        return $this;
     }
 
-    //GETTER ET SETTER
-
-    //METHOD
-    public function display(){
-        echo '<!DOCTYPE html>
-            <html lang="fr">
-            <head>
-                <meta charset="UTF-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <title>'.$this->title.'</title>
-                <link rel="stylesheet" href="./public/src/css/style.css">
-                <script src="'. $this->linkScript .'" defer></script>
-            </head>
-            <body>
-                <header>
-                    <nav>
-                        <a href='.$_ENV['utilisateurs'].'>Utilisateurs</a>
-                        <a href='.$_ENV['articles'].'>Articles</a>
-                    </nav>
-                </header>';
-                }
-            }
-
-?>
+    public function display(): void
+    {
+        echo $this->buffer ?? '';
+    }
+}
