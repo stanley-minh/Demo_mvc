33208c0e00e79a2195cff3711191b31267f7a67c
Kellyan01 <depriester.yoann@neuf.fr>
2026-08-25 09:15:26 +0200

    Add Article Controller + Model

--- model/modelArticle.php
+++ model/modelArticle.php
@@ -1,18 +1,36 @@
 <?php
-function getArticles(){
-    try{
-        $bdd = connect();
-        //1. Preparer la requête
-        $request = 'SELECT a.title, a.content, a.created_at, a.edited_at, u.pseudo FROM article a INNER JOIN user u ON u.id = a.user_id';
+class ModelArticle{
+    //ATTRIBUT
+    private ?int $id;
+    private ?string $title;
+    private ?string $content;
+    private ?string $createdAt;
+    private ?string $editedAt;
+    private ?string $author;
+    private PDO $bdd;
 
-        $req = $bdd->prepare($request);
+    //CONSTRUCTEUR
+    public function __construct(PDO $bdd){
+        $this->bdd = $bdd;
+    }
 
-        //2. Exécution de la requête
-        $req->execute();
+    //GETTER ET SETTER
 
-        //3. Retourner les données
-        return $req->fetchAll(PDO::FETCH_ASSOC);
-    }catch(EXCEPTION $error){
-        die($error->getMessage());
+    //METHODS
+    public function findAll():?array{
+        try{
+            //1. Preparer la requête
+            $request = 'SELECT a.id, a.title, a.content, a.created_at, a.edited_at, u.pseudo FROM article a INNER JOIN user u ON u.id = a.user_id';
+
+            $req = $this->bdd->prepare($request);
+
+            //2. Exécution de la requête
+            $req->execute();
+
+            //3. Retourner les données
+            return $req->fetchAll(PDO::FETCH_ASSOC);
+        }catch(EXCEPTION $error){
+            die($error->getMessage());
+        }
     }
 }
