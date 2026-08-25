c9552d88b9870f1a5b1844c19d7ee6689801faf3
stanley-minh <staley.bullet.pro@gmail.com>
2026-08-25 11:44:10 +0200

    Sync avec le dépôt du prof

--- index.php
+++ index.php
@@ -1,35 +1,42 @@
 <?php
-//ROUTEUR
-//import des ressources
-include('./env.php');
-include('./utils/utils.php');
-include('./Model/ModelUser.php');
-include('./Model/ModelArticle.php');
-include('./view/viewFooter.php');
-include('./view/viewHeader.php');
-include('./view/viewUser.php');
-include('./view/viewArticle.php');
-include('./Controller/ControllerUser.php');
-include('./Controller/ControllerArticle.php');
+//Déclaration de ma variable d'affichage
+$listeUtilisateur = '';
 
-//1. Récupérer l'url demandé par l'utilisateur
-$url = parse_url($_SERVER['REQUEST_URI']);
+//AFFICHER LES UTILISATEURS : pseudo, email, role
+//1. Connexion à la BDD avec l'objet PDO
+$bdd = new PDO('mysql:host=127.0.0.1:3306;dbname=mvc','root','root',[
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+]);
 
-//2. Récupérer le path de l'url : ceux qui vient après le nom de domaine
-$path = isset($url['path']) ? $url['path'] : '/';
+//2. Préparer une requête pour SELECT les utilisateurs
+$req = $bdd->prepare('SELECT pseudo, email, role FROM user INNER JOIN role ON role.id = user.role_id');
 
-//3. Appeler le Controller lié à la route demandée
-switch ($path) {
-    case '/':
-    case $_ENV['utilisateurs']:
-        $controller = new ControllerUser(new ModelUser(connect()), new ViewUser());
-        $controller->render();
-        break;
-    case $_ENV['articles'] :
-        $controller = new ControllerArticle(new ModelArticle(connect()), new ViewArticle());
-        $controller->render();
-        break;
-    default:
-        echo "erreur 404";
-        break;
+$req->execute();
+
+$data = $req->fetchAll(PDO::FETCH_ASSOC);
+
+//3. Traiter les données pour ensuite les afficher
+foreach($data as $row){
+    $listeUtilisateur .="<li>Pseudo :".$row['pseudo']." - Email : ".$row['email']." - Role :".$row['role']."</li>";
 }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header></header>
+    <main>
+        <h1>Liste des utilisateurs</h1>
+        <ul>
+            <?php echo $listeUtilisateur ?>
+        </ul>
+    </main>
+    <footer></footer>
+</body>
+</html>
